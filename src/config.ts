927bcbd08d6867561606967ecd1908357c19c0b5
mahmoud <Mahmoud.Elkhateb@proton.me>
2025-04-22 04:43:12 +0200

    START#1

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Amr Abdel Hamide",
-	subtitle: "Bad Glitch ",
+	title: "Mahmoud Elkhateb",
+	subtitle: "El5aetb",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/Bad-Glitch", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/elkhateb-22", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -54,24 +54,24 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Amr Abdel Hamide",
-	bio: "Red Teamer | OSCP, CRTP, eWAPTXv3 | breaking systems, simulating real threats.",
+	bio: "Bug Hunter, Breaking Systems | eJPTv2, eWAPTXv3",
 	links: [
 		{
 			name: "Twitter",
 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://x.com/BadGlitch0",
+			url: "https://x.com/el5ateb_22",
 		},
 		{
 			name: "Linkedin",
 			icon: "fa6-brands:linkedin-in",
-			url: "https://eg.linkedin.com/in/amr-abdel-hamide-4a8a28287",
+			url: "https://eg.linkedin.com/in/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/Bad-Glitch",
+			url: "https://github.com/elkhateb-22",
 		},
 	],
 };
